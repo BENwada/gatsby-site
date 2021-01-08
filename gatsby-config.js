f928a6cebe0da47dcdd4f9ad2001e12b32796e69
BENwada <domo0103wada@gmail.com>
2021-01-08 16:31:19 +0900

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,21 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "はじめてのGatsby Site",
+    author: "Reffect",
+    category: ["Laravel", "Vue.js", "React"],
+    user: { name: "John Doe", email: "john@example.com" },
+  },
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    "gatsby-transformer-remark",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,36 @@
 import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  return (
+    <div>
+      <Layout>
+        <h1>Gatsby Blog Site</h1>
+        {data.allMarkdownRemark.nodes.map(node => (
+          <div key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>{node.frontmatter.date}</p>
+            <div dangerouslySetInnerHTML={{ __html: node.html }} />
+          </div>
+        ))}
+      </Layout>
+    </div>
+  )
 }
+
+export const query = graphql`
+  {
+    allMarkdownRemark {
+      nodes {
+        id
+        html
+        timeToRead
+        frontmatter {
+          date
+          title
+        }
+      }
+    }
+  }
+`
